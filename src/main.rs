2ea9b84476d06aacc73a3b9edd820e1a7be142c4
Bryce <utbryceh@gmail.com>
2022-12-14 13:32:25 -0700

    implements incomplete rigidbody

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 mod objects;
+use objects::{RigidBody, Point, RectObject};
 
 fn main() {
-    println!("Hello, world!");
+    let object1 = RigidBody {
+        center: Point::new(10.0, 10.0),
+        width: 5.0,
+        height: 5.0,
+        density: 1.0,
+    };
+
+    let object2 = RigidBody {
+        center: Point::new(10.0, 10.0),
+        width: 4.0,
+        height: 4.0,
+        density: 1.0,
+    };
+
+    println!("{}", object1.collides_with(&object2));
 }
--- src/objects.rs
+++ src/objects.rs
@@ -5,8 +5,25 @@ pub struct Point {
 }
 
 impl Point {
-    fn new(x: f64, y: f64) -> Self {
+    pub fn new(x: f64, y: f64) -> Self {
         Point { x: x, y: y }
+    }
+
+    // makes set equal to passed
+    // in point
+    fn set(&mut self, other: &Point) {
+        self.x = other.x;
+        self.y = other.y;
+    }
+
+    // returns the sum of self and other
+    fn add(point1: &Point, point2: &Point) -> Point {
+        Point::new(point1.x + point2.x, point1.y + point2.y)
+    }
+
+    // adds self to other
+    fn add_to(&self, other: &mut Point) {
+        other.set(&Point::add(&self, &other));
     }
 }
 
@@ -27,37 +44,111 @@ pub trait RectObject {
     // value, and uppermost y value respectively
     fn bounds(&self) -> Vec<f64>;
 
-    // detects collision with the other object
-    // (colliding edges counts as collision)
-    fn collides_with(&self, other: &dyn RectObject) -> bool {
+    // detects if the there is collion on
+    // the horizontal axis
+    fn collides_with_y(&self, other: &dyn RectObject) -> bool {
         let self_bounds: Vec<f64> = self.bounds();
         let other_bounds: Vec<f64> = other.bounds();
 
-        let mut collide_y: bool = false;
-        let mut collide_x: bool = false;
+        let mut collides: bool = false;
 
         if sandwitch(other_bounds[0], self_bounds[0], other_bounds[1]) {
             // is self's leftmost side in other?
-            collide_x = true;
+            collides = true;
         } else if sandwitch(other_bounds[0], self_bounds[1], other_bounds[1]) {
             // is self's rightmost side in other?
-            collide_x = true;
+            collides = true;
         } else if self_bounds[0] <= other_bounds[0] && other_bounds[1] < self_bounds[1] {
             // do we completely contatain other on x axis?
-            collide_x = true;
+            collides = true;
         }
+
+        collides
+    }
+
+    // detects if the there is collion on
+    // the vertical axis
+    fn collides_with_x(&self, other: &dyn RectObject) -> bool {
+        let self_bounds: Vec<f64> = self.bounds();
+        let other_bounds: Vec<f64> = other.bounds();
+
+        let mut collides: bool = false;
 
         if sandwitch(other_bounds[2], self_bounds[2], other_bounds[3]) {
             // is self's bottom side in other?
-            collide_y = true;
+            collides = true;
         } else if sandwitch(other_bounds[2], self_bounds[3], other_bounds[3]) {
             // is self's top side in other?
-            collide_y = true;
+            collides = true;
         } else if self_bounds[2] <= other_bounds[2] && other_bounds[3] < self_bounds[3] {
             // do we completely contatain other on y axis?
-            collide_y = true;
+            collides = true;
         }
 
-        collide_x && collide_y
+        collides
+    }
+
+    // detects collision with the other object
+    // (colliding edges counts as collision)
+    fn collides_with(&self, other: &dyn RectObject) -> bool {
+        self.collides_with_x(other) && self.collides_with_y(other)
+    }
+}
+
+pub struct RigidBody {
+    pub center: Point,
+    pub width: f64,
+    pub height: f64,
+    pub density: f64,
+}
+
+impl RigidBody {
+    fn move_by(&mut self, movement: &Point) {
+        movement.add_to(&mut self.center);
+    }
+}
+
+impl RectObject for RigidBody {
+    fn points(&self) -> Vec<Point> {
+        let mut points: Vec<Point> = vec![];
+
+        let half_width: f64 = self.width / 2.0;
+        let half_height: f64 = self.height / 2.0;
+
+        // add points to vector
+        points.push(Point::new(
+            self.center.x - half_width,
+            self.center.y + half_height,
+        ));
+        points.push(Point::new(
+            self.center.x + half_width,
+            self.center.y + half_height,
+        ));
+        points.push(Point::new(
+            self.center.x + half_width,
+            self.center.y - half_height,
+        ));
+        points.push(Point::new(
+            self.center.x - half_width,
+            self.center.y - half_height,
+        ));
+
+        points
+    }
+
+    fn bounds(&self) -> Vec<f64> {
+        let mut points: Vec<f64> = vec![];
+
+        // push left x, right x
+        let half_width: f64 = self.width / 2.0;
+        points.push(self.center.x - half_width);
+        points.push(self.center.x + half_width);
+
+        // push bottom y, top y
+        let half_height: f64 = self.width / 2.0;
+        points.push(self.center.y - half_height);
+        points.push(self.center.y + half_height);
+
+        points
     }
 }
