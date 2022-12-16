896b29809adad6292dd2d2fbd2805be342b75928
Bryce <utbryceh@gmail.com>
2022-12-16 10:03:02 -0700

    graphics window working

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,40 @@
+extern crate minifb;
+use minifb::{Key, Window, WindowOptions};
+
 mod objects;
 
-use objects::*;
+const WINDOW_WIDTH: usize = 0xff * 4;
+const WINDOW_HEIGHT: usize = 0xff * 3;
 
 fn main() {
-    let mut object = MovingObject::new(Vector2::new(0.0, 0.0), Vector2::new(1.0, 1.0), 3.0, 1.0, false);
-
-    object.update(1.2);
-
-    println!("x: {}, y: {}, direction: {}", object.center.x, object.center.y, match object.direction {
-        MovingObjectDirections::Leaving => "Leaving",
-        MovingObjectDirections::Returning => "Returning",
-        _ => "Standstill",
+    let mut window = Window::new(
+        "Platformer - ESC to exit",
+        WINDOW_WIDTH,
+        WINDOW_HEIGHT,
+        WindowOptions::default(),
+    )
+    .unwrap_or_else(|error| {
+        panic!("Error opening window: {}", error);
     });
+
+    // limit fps to 60
+    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+
+    // this will be where we write out pixel values
+    let mut buffer: Vec<u32> = vec![0; WINDOW_WIDTH * WINDOW_HEIGHT];
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        for x in 0..WINDOW_WIDTH {
+            for y in 0..WINDOW_HEIGHT {
+                buffer[y * WINDOW_WIDTH + x] = (x / 4 * 0x10000 + y / 3 + (WINDOW_WIDTH - x) / 4 * 0x100) as u32;
+            }
+        }
+
+        // update our window with our pixel values
+        window
+            .update_with_buffer(&buffer, WINDOW_WIDTH, WINDOW_HEIGHT)
+            .unwrap_or_else(|error| {
+                panic!("Error updating window: {}", error);
+            });
+    }
 }
