80df2f34695cb09193e380c02ab4b3bddd60a7c9
Bryce <utbryceh@gmail.com>
2022-12-16 20:25:08 -0700

    makes code more maintainable

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,45 @@ use objects::{contains_point_cache_bounds, RectObject, RigidBody, StaticObject, 
 const WINDOW_WIDTH: usize = 0xff * 4;
 const WINDOW_HEIGHT: usize = 0xff * 3;
 
+const PLAYER_WALKING_SPEED: f64 = 3.2;
+const PLAYER_RUNNING_SPEED: f64 = 5.0;
+
+const JUMP_BUFFER_SECS: f64 = 0.04;
+const JUMP_FORCE: f64 = 4.7;
+
+const GRAVITY_MOVING_UP: f64 = 1.0 / 4.6;
+const GRAVITY_MOVING_DOWN: f64 = 1.0 / 11.0;
+const GRAVITY_ON_OBJECT: f64 = -1.0 / 3.0;
+
+const FRAME_LIMIT_MILLIS: u64 = 11;
+
+const DIRECTIONAL_COLLISION_DEPTH: f64 = 2.0;
+
 fn main() {
+    // our player
+    let mut player = RigidBody {
+        center: Vector2::new(255.0, 300.0),
+        width: 20.0,
+        height: 40.0,
+
+        velocity: Vector2::new(0.0, 0.0),
+        density: 0.0,
+        static_friction: false,
+    };
+
+    let static_objects = vec![
+        StaticObject {
+            center: Vector2::new(510.0, 75.0),
+            width: 700.0,
+            height: 150.0,
+        },
+        StaticObject {
+            center: Vector2::new(500.0, 200.0),
+            width: 50.0,
+            height: 50.0,
+        },
+    ];
+
     // our window :)
     let mut window = Window::new(
         "Platformer - ESC to exit",
@@ -19,8 +57,10 @@ fn main() {
         panic!("Error opening window: {}", error);
     });
 
-    // limit fps to 60
-    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+    // configure the window
+    window.limit_update_rate(Some(std::time::Duration::from_millis(FRAME_LIMIT_MILLIS)));
+    window.topmost(true);
+    window.set_position(20, 20);
 
     // this will be where we write out pixel values
     let mut window_buffer: Vec<u32> = vec![0; WINDOW_WIDTH * WINDOW_HEIGHT];
@@ -28,27 +68,15 @@ fn main() {
     // how long each frame takes (in seconds)
     let mut frame_time: f64 = 0.0;
 
-    // our player
-    let mut player = RigidBody {
-        center: Vector2::new(255.0, 520.0),
-        width: 20.0,
-        height: 40.0,
-
-        velocity: Vector2::new(0.0, 0.0),
-        density: 0.0,
-        static_friction: false,
-    };
-
-    // grounds
-    let ground = StaticObject {
-        center: Vector2::new(510.0, 75.0),
-        width: 1020.0,
-        height: 150.0,
-    };
-
     // cache object bounds
-    let ground_bounds = ground.bounds();
+    let static_object_bounds: Vec<Vec<f64>> = static_objects
+        .iter()
+        .map(|object| object.bounds())
+        .collect();
     let mut player_bounds;
+
+    // jump buffers make movement feel a little better
+    let mut jump_buffer: f64 = 0.0;
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
         // recache player bounds
@@ -57,39 +85,72 @@ fn main() {
         // used to measure the frame time
         let now = std::time::Instant::now();
 
-        // physics
-        player.velocity.y -= frame_time / 20.0;
+        // apply gravity
+        player.velocity.y -= match player.velocity.y <= 0.0 {
+            false => GRAVITY_MOVING_UP,
+            true => GRAVITY_MOVING_DOWN,
+        };
 
         // calculate how far the player moves
         let movement_vector = &Vector2::multiply(&player.velocity, frame_time);
+        player.move_by(&movement_vector);
 
-        // do all moving and physics only if the window is active
-        // so that physics don't break
-        if window.is_active() {
-            player.move_by(&movement_vector);
+        let current_speed: f64 = match window.is_key_down(Key::LeftShift) {
+            false => PLAYER_WALKING_SPEED,
+            true => PLAYER_RUNNING_SPEED,
+        };
 
-            if window.is_key_down(Key::A) {
-                player.center.x -= 5.0 * frame_time;
-            }
-            if window.is_key_down(Key::D) {
-                player.center.x += 5.0 * frame_time;
+        if window.is_key_down(Key::A) {
+            player.center.x -= current_speed * frame_time;
+        }
+        if window.is_key_down(Key::D) {
+            player.center.x += current_speed * frame_time;
+        }
+
+        let mut on_object: bool = false;
+
+        // handle collisions
+        for i in 0..static_objects.len() {
+            let bounds = &static_object_bounds[i];
+
+            // if we collide with the object, decide the best
+            // way to move ourselves outside of the object
+            if player.collides_with(&static_objects[i]) {
+                if player_bounds[2] >= bounds[3] - DIRECTIONAL_COLLISION_DEPTH {
+                    player.center.y = bounds[3] + player.height / 2.0;
+
+                    on_object = true;
+                }
+
+                if player_bounds[3] <= bounds[2] + DIRECTIONAL_COLLISION_DEPTH {
+                    player.center.y = bounds[2] - player.height / 2.0;
+                }
+
+                if player_bounds[0] >= bounds[1] - DIRECTIONAL_COLLISION_DEPTH {
+                    player.center.x = bounds[1] + player.width / 2.0;
+                }
+
+                if player_bounds[1] <= bounds[0] + DIRECTIONAL_COLLISION_DEPTH {
+                    player.center.x = bounds[0] - player.width / 2.0;
+                }
             }
         }
 
-        let mut on_ground: bool = false;
-
-        // if the player is on the ground, move it out of the ground
-        if player.collides_with(&ground) {
-            // if we collide with the ground, put ourselves on it
-            player.center.y = ground_bounds[3] + player.height / 2.0;
-            player.velocity = Vector2::new(0.0, 0.0);
-
-            on_ground = true;
+        // decrease our jump buffer time if it's counting down
+        if jump_buffer > 0.0 {
+            jump_buffer -= frame_time;
         }
 
-        // jump if space is pressed and window is focused
-        if on_ground && window.is_key_down(Key::Space) && window.is_active() {
-            player.velocity.y = 2.5;
+        // if space is pressed, start jump buffer
+        if window.is_key_pressed(Key::Space, minifb::KeyRepeat::No) || window.is_key_down(Key::W) {
+            jump_buffer = JUMP_BUFFER_SECS;
+        }
+
+        if on_object && jump_buffer > 0.0 {
+            player.velocity.y = JUMP_FORCE;
+            jump_buffer = 0.0;
+        } else if on_object {
+            player.velocity.y = GRAVITY_ON_OBJECT;
         }
 
         // render each pixel
@@ -98,9 +159,17 @@ fn main() {
                 let rgb: u32;
                 let point = Vector2::new(x as f64, (WINDOW_HEIGHT - y) as f64);
 
+                let mut static_object_collision: bool = false;
+
+                for bounds in &static_object_bounds {
+                    if contains_point_cache_bounds(&point, &bounds) {
+                        static_object_collision = true;
+                    }
+                }
+
                 if contains_point_cache_bounds(&point, &player_bounds) {
                     rgb = 0xff0000;
-                } else if contains_point_cache_bounds(&point, &ground_bounds) {
+                } else if static_object_collision {
                     rgb = 0xff;
                 } else {
                     rgb = 0x200020;
