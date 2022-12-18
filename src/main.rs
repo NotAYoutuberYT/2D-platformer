6b2967e63f9e5a301bc1d4e9d93ba31e3088821c
NotAYoutuberYT <NotAYoutuberYT>
2022-12-17 21:57:59 -0700

    adds camera movement

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate minifb;
+
 use minifb::{Key, Window, WindowOptions};
 
 mod objects;
@@ -13,8 +14,12 @@ const FPS: f64 = 144.0;
 const PLAYER_WALKING_SPEED: f64 = 3.2;
 const PLAYER_RUNNING_SPEED: f64 = 5.0;
 
-const PERCENT_SCREEN_PLAYER_IN: f64 = 20.0;
-const CAMERA_MOVING_EASING: f64 = 1.0 / 150.0;
+const PERCENT_SCREEN_PLAYER_IN_X: f64 = 18.0;
+const PERCENT_SCREEN_PLAYER_IN_Y: f64 = 22.0;
+const PLAYER_FOCUS_X_OFFSET: f64 = 0.0;
+const PLAYER_FOCUS_Y_OFFSET: f64 = -200.0;
+const CAMERA_MOVING_EASING_X: f64 = 1.0 / 250.0;
+const CAMERA_MOVING_EASING_Y: f64 = 1.0 / 350.0;
 
 const JUMP_BUFFER_HUNDRETHSECS: f64 = 0.0005;
 const JUMP_FORCE: f64 = 5.0;
@@ -27,16 +32,24 @@ const VERTICAL_VELOCITY_ON_OR_UNDER_OBJECT: f64 = -1.0 / 2.5;
 // but will make player snap to the edges of platforms
 const COLLISION_DEPTH_BASE: f64 = 2.0;
 
-const MIN_X_FROM_CAMERA: f64 = WINDOW_WIDTH as f64 / 2.0 - PERCENT_SCREEN_PLAYER_IN / 200.0 * WINDOW_WIDTH as f64;
-const MAX_X_FROM_CAMERA: f64 = WINDOW_WIDTH as f64 / 2.0 + PERCENT_SCREEN_PLAYER_IN / 200.0 * WINDOW_WIDTH as f64;
+const MIN_X_FROM_CAMERA: f64 = WINDOW_WIDTH as f64 / 2.0
+    - PERCENT_SCREEN_PLAYER_IN_X / 200.0 * WINDOW_WIDTH as f64
+    + PLAYER_FOCUS_X_OFFSET;
+const MAX_X_FROM_CAMERA: f64 = WINDOW_WIDTH as f64 / 2.0
+    + PERCENT_SCREEN_PLAYER_IN_X / 200.0 * WINDOW_WIDTH as f64
+    + PLAYER_FOCUS_X_OFFSET;
+const MIN_Y_FROM_CAMERA: f64 = WINDOW_WIDTH as f64 / 2.0
+    - PERCENT_SCREEN_PLAYER_IN_Y / 200.0 * WINDOW_HEIGHT as f64
+    + PLAYER_FOCUS_Y_OFFSET;
+const MAX_Y_FROM_CAMERA: f64 = WINDOW_WIDTH as f64 / 2.0
+    + PERCENT_SCREEN_PLAYER_IN_Y / 200.0 * WINDOW_HEIGHT as f64
+    + PLAYER_FOCUS_Y_OFFSET;
 
 // don't touch these consts, they're decided by those above
 
 const FRAME_LIMIT_MILLIS: u64 = (1000.0 / FPS) as u64;
 
 fn main() {
-    let mut camera_bottom_left: Vector2 = Vector2::new(0.0, 0.0);
-
     // our player
     let mut player = RigidBody {
         center: Vector2::new(400.0, 300.0),
@@ -50,9 +63,9 @@ fn main() {
 
     let static_objects = [
         StaticObject {
-            center: Vector2::new(510.0, 75.0),
-            width: 700.0,
-            height: 150.0,
+            center: Vector2::new(510.0, 50.0),
+            width: 1750.0,
+            height: 200.0,
         },
         StaticObject {
             center: Vector2::new(430.0, 185.0),
@@ -69,6 +82,26 @@ fn main() {
             width: 50.0,
             height: 50.0,
         },
+        StaticObject {
+            center: Vector2::new(407.5, 675.0),
+            width: 45.0,
+            height: 20.0,
+        },
+        StaticObject {
+            center: Vector2::new(-1000.0, 500.0),
+            width: 50.0,
+            height: 50.0,
+        },
+        StaticObject {
+            center: Vector2::new(-1125.0, 500.0),
+            width: 50.0,
+            height: 50.0,
+        },
+        StaticObject {
+            center: Vector2::new(-1025.0, 300.0),
+            width: 200.0,
+            height: 50.0,
+        },
     ];
 
     let mut moving_objects = [
@@ -77,15 +110,23 @@ fn main() {
             Vector2::new(265.0, 575.0),
             80.0,
             35.0,
-            220.0,
+            150.0,
             false,
         ),
         MovingObject::new(
-            Vector2::new(600.0, 390.0),
-            Vector2::new(800.0, 545.0),
+            Vector2::new(575.0, 390.0),
+            Vector2::new(775.0, 545.0),
             100.0,
             35.0,
-            220.0,
+            150.0,
+            false,
+        ),
+        MovingObject::new(
+            Vector2::new(200.0, 770.0),
+            Vector2::new(-1000.0, 60.0),
+            150.0,
+            30.0,
+            270.0,
             false,
         ),
     ];
@@ -100,6 +141,10 @@ fn main() {
     .unwrap_or_else(|error| {
         panic!("Error opening window: {}", error);
     });
+
+    // this will store the location of the bottom left corner of the rendring window
+    let mut camera_bottom_left: Vector2 =
+        Vector2::new(player.center.x - WINDOW_WIDTH as f64 / 2.0, 0.0);
 
     // configure the window
     window.limit_update_rate(Some(std::time::Duration::from_millis(FRAME_LIMIT_MILLIS)));
@@ -155,10 +200,11 @@ fn main() {
         player_bounds = player.bounds();
 
         // find movement speed
-        let current_speed: f64 = match window.is_key_down(Key::LeftShift) || window.is_key_down(Key::RightShift) {
-            false => PLAYER_WALKING_SPEED,
-            true => PLAYER_RUNNING_SPEED,
-        };
+        let current_speed: f64 =
+            match window.is_key_down(Key::LeftShift) || window.is_key_down(Key::RightShift) {
+                false => PLAYER_WALKING_SPEED,
+                true => PLAYER_RUNNING_SPEED,
+            };
 
         // apply movement speed
         if window.is_key_down(Key::A) {
@@ -283,9 +329,43 @@ fn main() {
 
         // move our camera
         if player.center.x - camera_bottom_left.x < MIN_X_FROM_CAMERA {
-            camera_bottom_left.x += (camera_bottom_left.x - MIN_X_FROM_CAMERA) * frame_time * CAMERA_MOVING_EASING;
+            camera_bottom_left.x -= (player.center.x - camera_bottom_left.x - MIN_X_FROM_CAMERA)
+                * (player.center.x - camera_bottom_left.x - MIN_X_FROM_CAMERA)
+                * frame_time
+                * CAMERA_MOVING_EASING_X;
+
+            if player.center.x - camera_bottom_left.x > MIN_X_FROM_CAMERA {
+                camera_bottom_left.x = player.center.x - MIN_X_FROM_CAMERA;
+            }
         } else if player.center.x - camera_bottom_left.x > MAX_X_FROM_CAMERA {
-            camera_bottom_left.x += (MIN_X_FROM_CAMERA - camera_bottom_left.x) * frame_time * CAMERA_MOVING_EASING;
+            camera_bottom_left.x += (player.center.x - camera_bottom_left.x - MAX_X_FROM_CAMERA)
+                * (player.center.x - camera_bottom_left.x - MAX_X_FROM_CAMERA)
+                * frame_time
+                * CAMERA_MOVING_EASING_X;
+
+            if player.center.x - camera_bottom_left.x < MAX_X_FROM_CAMERA {
+                camera_bottom_left.x = player.center.x - MAX_X_FROM_CAMERA;
+            }
+        }
+
+        if player.center.y - camera_bottom_left.y < MIN_Y_FROM_CAMERA {
+            camera_bottom_left.y -= (player.center.y - camera_bottom_left.y - MIN_Y_FROM_CAMERA)
+                * (player.center.y - camera_bottom_left.y - MIN_Y_FROM_CAMERA)
+                * frame_time
+                * CAMERA_MOVING_EASING_X;
+
+            if player.center.y - camera_bottom_left.y > MIN_Y_FROM_CAMERA {
+                camera_bottom_left.y = player.center.y - MIN_Y_FROM_CAMERA;
+            }
+        } else if player.center.y - camera_bottom_left.y > MAX_Y_FROM_CAMERA {
+            camera_bottom_left.y += (player.center.y - camera_bottom_left.y - MAX_Y_FROM_CAMERA)
+                * (player.center.y - camera_bottom_left.y - MAX_Y_FROM_CAMERA)
+                * frame_time
+                * CAMERA_MOVING_EASING_Y;
+
+            if player.center.y - camera_bottom_left.y < MAX_Y_FROM_CAMERA {
+                camera_bottom_left.y = player.center.y - MAX_Y_FROM_CAMERA;
+            }
         }
 
         //
@@ -295,7 +375,10 @@ fn main() {
         for x in 0..WINDOW_WIDTH {
             for y in 0..WINDOW_HEIGHT {
                 let rgb: u32;
-                let world_point = Vector2::new(camera_bottom_left.x + x as f64, camera_bottom_left.y + (WINDOW_HEIGHT - y) as f64);
+                let world_point = Vector2::new(
+                    camera_bottom_left.x + x as f64,
+                    camera_bottom_left.y + (WINDOW_HEIGHT - y) as f64,
+                );
 
                 let mut static_object_collision: bool = false;
                 let mut moving_object_collision: bool = false;
